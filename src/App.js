c37afa8ad2a9dd007f4017d9ee0d49687e814eb9
Timothy Bedsole <timbedsole@gmail.com>
2018-09-19 00:17:42 -0400

    Rendering header to the view

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          
+          <Header />
         </div>
       </Router>
     );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+  <div className="container-fluid">
+    <header className="row">
+      <div className="col-md-12 text-center">
+        <h1>New York Times Article Scrubber</h1>
+        <h4>Search for and annotate articles of interest!</h4>
+      </div>
+    </header>
+  </div>
+)};
+
+export default Header;
