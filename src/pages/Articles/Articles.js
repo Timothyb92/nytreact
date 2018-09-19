a3f1399e7027d2cc140ef7768ed25ba2d46e529f
Timothy Bedsole <timbedsole@gmail.com>
2018-09-19 00:58:53 -0400

    Added saveArticle method to API

--- src/pages/Articles/Articles.js
+++ src/pages/Articles/Articles.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Results from '../../components/Results';
 import Saved from '../../components/Saved';
 import Search from '../../components/Search';
+import API from '../../utils/API';
 
 class Articles extends Component {
   state = {
@@ -10,6 +11,39 @@ class Articles extends Component {
     date: '',
     url: ''
   };
+
+  componentDidMount() {
+    this.loadArticles();
+  }
+
+  loadArticles = () => {
+    API.getSavedArticles()
+    .then(data => {
+      this.setState({ articles: data.data, title: '', date: '', url: '' })
+    })
+    .catch(err => console.log(err));
+  };
+
+  handleInputChange = () => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.title) {
+      API.saveArticle({
+        title: this.state.title,
+        date: this.state.date,
+        url: this.state.url
+      })
+      .then(res => this.loadArticles())
+      .catch(err => console.log(err));
+    }
+  };
+
 };
 
 export default Articles;
--- src/utils/API.js
+++ src/utils/API.js
@@ -3,5 +3,7 @@ import axios from 'axios';
 export default {
   getSavedArticles: (id) => axios.get(`api/articles/${id}`),
 
-  removeSavedArticle: (id) => axios.delete(`api/articles/${id}`)
+  removeSavedArticle: (id) => axios.delete(`api/articles/${id}`),
+
+  saveArticle: (article) => axios.put('/api/articles/', article)
 };
